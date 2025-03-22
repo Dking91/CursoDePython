4a07971e1280cf5e36e91a9f478c9a82336f388f
Edson Silva <edson23jpa@gmail.com>
2025-03-22 20:18:02 -0300

    atualização

---
+++ Aula07.py
@@ -0,0 +1,5 @@
+# + ( soma), - ( subtração), * (multiplicaçõa0, / (divisão), ** (Potência), // (divisão inteira), % (resto da divisão)
+# = (atribuição), == (Igual)
+
+#Ordem de precedencia
+#1- (), 2- **, 3- ( *, /, **, % ), 4- ( +, -)
