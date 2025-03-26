0f98d30cca68300ece0d62707ecd03ce82b241bc
Edson Silva <edson23jpa@gmail.com>
2025-03-25 23:11:58 -0300

    Introdução de exercicios

--- ex016.py
+++ ex016.py
@@ -1,7 +1,9 @@
 '''Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
 
 '''
-from math import trunc
+
+'''Não foi necessario importar uma biblioteco pois é possivel usar a conversão de variavel int'''
+#from math import trunc
 
 num = float(input('Digite um valor: '))
-print(' O valor digitado foi {} e a sua porção é {}'.format(num, trunc(num)))
+print(' O valor digitado foi {} e a sua porção é {}'.format(num, int(num)))
