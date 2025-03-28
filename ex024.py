f83ce9e364a63c3364c40d6f0dd41d1b980c67b1
Edson Silva <edson23jpa@gmail.com>
2025-03-28 00:19:39 -0300

    Introdução de exercicios

--- ex024.py
+++ ex024.py
@@ -1,1 +1,5 @@
+'''Crie um programa que leia o nome de uma cidade diga se ela
+começa ou não com o nome “SANTO”'''
 
+cid = str(input('Digite a cidade que você nasceu: ')).strip()
+print(cid[:3].title() == 'Rio')
